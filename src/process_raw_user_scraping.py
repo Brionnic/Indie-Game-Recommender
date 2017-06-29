de9b58475db2801c6665ad2e84d61fd49a34910d
Brian Hardenstein <bhardens@yahoo.com>
2017-06-29 16:45:09 -0700

    fix previous commit

--- src/process_raw_user_scraping.py
+++ src/process_raw_user_scraping.py
@@ -48,58 +48,63 @@ def get_userinfo_from_doc(document):
     '''
     data = document["data"].lower()
 
-    # remove useless front part of data
-    data = data[data.index("var rggames = [".lower()):(3+data.index("}}];".lower()))]
+    try:
+        # remove useless front part of data
+        data = data[data.index("var rggames = [".lower()):(3+data.index("}}];".lower()))]
 
-    # attempt to split the block of game info into sub-blocks specific to each game
-    raw_user_game_data = data.split("},{")
+        # attempt to split the block of game info into sub-blocks specific to each game
+        raw_user_game_data = data.split("},{")
 
-    # be overly explicit in where each variable will be stored which will assemble
-    # the dictionary specific for each user's game playtime, name, etc
-#     _name = ""
-#     _appid = ""
-#     _hours_played = ""
-#     _last_played = ""
+        # be overly explicit in where each variable will be stored which will assemble
+        # the dictionary specific for each user's game playtime, name, etc
+    #     _name = ""
+    #     _appid = ""
+    #     _hours_played = ""
+    #     _last_played = ""
 
-    # make a list to hold the individual game info dictionaries
-    game_info = []
+        # make a list to hold the individual game info dictionaries
+        game_info = []
 
-    # raw_user_data is a list of game data so walk over that and process each
-    # item into a dictionary to be added to the overall user profile
-    for raw_game_info in raw_user_game_data:
+        # raw_user_data is a list of game data so walk over that and process each
+        # item into a dictionary to be added to the overall user profile
+        for raw_game_info in raw_user_game_data:
 
-        # split the individual raw_game_info into each specific attribute
-        raw_game_attributes = raw_game_info.split(",")
+            # split the individual raw_game_info into each specific attribute
+            raw_game_attributes = raw_game_info.split(",")
 
-        # walk all of the attributes
-        for item in raw_game_attributes:
-#             print attribute
+            # walk all of the attributes
+            for item in raw_game_attributes:
+    #             print attribute
 
-            if "appid" in item:
-                _appid = item.split(":")[1]
+                if "appid" in item:
+                    _appid = item.split(":")[1]
 
-            if "name" in item:
-                _name = item[7:]
+                if "name" in item:
+                    _name = item[7:]
 
-            if "hours_forever" in item:
-                _hours_played = float(item.split(":")[1].strip('"'))
+                if "hours_forever" in item:
+                    _hours_played = float(item.split(":")[1].strip('"'))
 
-            if "last_played" in item:
-                _last_played = int(item.split(":")[1])
+                if "last_played" in item:
+                    _last_played = int(item.split(":")[1])
 
-        this_game_info_dict = {"game_name":_name, "app_id":_appid, "hours_played":_hours_played, "last_played":_last_played}
-        #print
-        game_info.append(this_game_info_dict)
+            this_game_info_dict = {"game_name":_name, "app_id":_appid, "hours_played":_hours_played, "last_played":_last_played}
+            #print
+            game_info.append(this_game_info_dict)
 
-        #print this_game_info_dict
+            #print this_game_info_dict
 
-        #print sub_info
+            #print sub_info
 
-    # make the total player game data dictionary:
-    player_dict = {"player":document["user"], "game_data": game_info}
+        # make the total player game data dictionary:
+        player_dict = {"player":document["user"], "game_data": game_info}
 
-    # return to caller
-    return player_dict
+        # return to caller
+        return player_dict
+
+    except Exception, e:
+        print "Failed to process user_page:", document["user"]
+        return None
 
 
 def digest_user_info_into_collection(source_collection, destination_collection):
@@ -111,6 +116,7 @@ def digest_user_info_into_collection(source_collection, destination_collection):
 
     start_size = destination_collection.find().count()
     print "Length of destination collection at start:", start_size
+    print
 
 
     # pause for a few minutes so user can see starting size of collection
@@ -122,13 +128,16 @@ def digest_user_info_into_collection(source_collection, destination_collection):
         # extract data from raw web page into a dictionary
         temp_data = get_userinfo_from_doc(doc)
 
-        # add the cleaned data to the proper collection
-        insert(destination_collection, temp_data, "player")
+        # make sure that processing didn't fail
+        if temp_data != None:
+            # add the cleaned data to the proper collection
+            insert(destination_collection, temp_data, "player")
 
     # after the work is done check the finished size
     final_size = destination_collection.find().count()
 
     # print how many items were added to the collection
+    print
     print "Added", final_size - start_size, "to the collection."
 
 if __name__ == '__main__':
