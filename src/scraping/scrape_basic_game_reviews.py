430471098c9e2b7724a0a616c0755b42aa3b97eb
Brian Hardenstein <bhardens@yahoo.com>
2017-07-12 14:10:49 -0700

    cleaning some directories

---
+++ src/scraping/scrape_basic_game_reviews.py
@@ -0,0 +1,15 @@
+# import MongoDB modules
+from pymongo import MongoClient
+
+# import the Requests HTTP library
+import requests
+
+# import the Beautiful Soup module
+from bs4 import BeautifulSoup
+
+# import the time module for the sleep functionality
+# in order to be polite when scraping
+import time
+
+# randomize a bit for the scraping
+import random
---
+++ src/scrapped/run_GR.py
@@ -0,0 +1,21 @@
+# to use pandas dataframes
+import pandas as pd
+
+import numpy as np
+
+# import MongoDB modules
+from pymongo import MongoClient
+
+# we can always use more time
+import time
+
+# import the class that handles this stuff
+from GameRecommender import GameRecommender
+
+if __name__ == "__main__":
+
+    recommender = GameRecommender()
+
+    model = recommender.fit()
+
+    predictions = recommender.transform()
